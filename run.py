9915b08eee02d02ea1b5ce6699bb4225b1f5361c
Omkar Kirpan <okirpan@gmail.com>
2020-01-09 18:43:38 +0530

    heroku deploy

--- run.py
+++ run.py
@@ -5,9 +5,11 @@ import os
 
 if __name__ == '__main__':
     conf = {
-        '/': {
+        'global': {
             'server.socket_host': '0.0.0.0',
-            'server.socket_port': int(os.environ.get('PORT', '5000')),
+            'server.socket_port': int(os.environ.get('PORT', 5000)),
+        },
+        '/': {
             'tools.sessions.on': True,
             'tools.staticdir.root': os.path.abspath(os.getcwd())
         },
