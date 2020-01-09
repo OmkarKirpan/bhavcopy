a61c5fd46570729a47d15690261994d5ec5f80af
Omkar Kirpan <okirpan@gmail.com>
2020-01-09 15:20:41 +0530

    worker added Document object model added redis set, get data done routes added

--- bhavcopy/controller.py
+++ bhavcopy/controller.py
@@ -3,7 +3,7 @@ import re
 import jinja2
 from datetime import datetime, timedelta
 
-from bhavcopy import bse
+from bhavcopy import bse, worker
 
 
 class BhavController(object):
@@ -13,7 +13,26 @@ class BhavController(object):
     )
 
     @cherrypy.expose
-    def index(self, date_str=None):
+    def index(self, row_size=10, date_str=None, error=None):
+
+        if date_str is None:
+            date = datetime.today() - timedelta(1)
+        else:
+            date = datetime.strptime(date_str, '%d%m%y')
+
+        date_text = date.strftime('%d, %B %Y')
+
+        try:
+            equities = worker.DAO().get_equities(date=date)
+        except worker.RedisDataNotFoundException:
+            raise cherrypy.HTTPError(message="Data not found in database")
+
+        for equity in equities[:row_size]:
+            print(equity.name + " " + str(equity.open) + " " + repr(equity.date))
+        return "OK."
+
+    @cherrypy.expose
+    def update(self, date_str=None):
 
         if date_str is None:
             date = datetime.today() - timedelta(1)
@@ -24,8 +43,9 @@ class BhavController(object):
             equities = bse.fetch_bhav(date)
         except bse.BhavNotFoundException:
             return "Bhav not found on BSE for " + date.strftime('%d-%m-%y') + ". Try with another date."
-
+        dao = worker.DAO()
         for equity in equities:
             print(equity.name + " " + str(equity.open) + " " + repr(equity.date))
+            dao.insert_equity(equity)
 
         return "OK."
---
+++ bhavcopy/worker.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+
+import redis
+
+from bhavcopy import model
+
+
+class RedisDataNotFoundException(BaseException):
+    pass
+
+
+class DAO:
+
+    def __init__(self, redis_host="localhost", redis_port=6379, redis_password=""):
+
+        self.db = redis.StrictRedis(
+            host=redis_host, port=redis_port, password=redis_password, decode_responses=True)
+
+    def insert_equity(self, equity):
+
+        key = 'equity:' + equity.name + ':' + equity.date.strftime('%d%m%y')
+
+        self.db.hset(key, 'code', equity.code)
+        self.db.hset(key, 'open', equity.open)
+        self.db.hset(key, 'high', equity.high)
+        self.db.hset(key, 'low', equity.low)
+        self.db.hset(key, 'close', equity.close)
+
+    def get_equities(self, name='*', date=None):
+        if date is None:
+            query = 'equity:' + name + ':*'
+        else:
+            query = 'equity:' + name + ':' + date.strftime('%d%m%y')
+
+        keys = self.db.keys(query)
+
+        if not keys:
+            raise RedisDataNotFoundException
+
+        equities = []
+
+        for key in keys:
+            hashval = self.db.hgetall(key)
+
+            name, date = key.split(':')[1], datetime.strptime(
+                key.split(':')[2], '%d%m%y')
+
+            equity = model.Equity(code=int(hashval['code']),
+                                  name=name,
+                                  open=float(hashval['open']),
+                                  high=float(hashval['high']),
+                                  low=float(hashval['low']),
+                                  close=float(hashval['close']),
+                                  date=date)
+
+            equities.append(equity)
+
+        return equities
